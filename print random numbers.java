926d1465cb8a332a4af430ab340f7ddc7ffc484b
Naveen kumar M <vh12080_it22@velhightech.com>
2025-01-28 21:37:04 +0530

    print random numbers.java

---
+++ print random numbers.java
@@ -0,0 +1,13 @@
+// print 2 numbers randomly
+
+import java.util.*;
+class randomNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int num1 = random.nextInt(100) + 1;
+        int num2 = random.nextInt(100) + 1;
+        int sum = num1 + num2;
+        System.out.println("Numbers are: " + num1 + " and " + num2);
+        System.out.println("Sum: " + sum);
+    }
+}
