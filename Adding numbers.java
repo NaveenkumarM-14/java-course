4572016c1b8e8e931f8155816375f6e4b97dfe45
Naveen kumar M <vh12080_it22@velhightech.com>
2025-01-28 21:49:39 +0530

    Adding numbers.java

---
+++ Adding numbers.java
@@ -0,0 +1,9 @@
+// Adding 2 numbers
+pubic class sum{
+  public static void main(String []args){
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    System.out.println("Sum is: "+sum);
+  }
+}
