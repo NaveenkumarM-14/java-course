1f40a423dec03de73e92b03717bc0d54615cb47f
Naveen kumar M <vh12080_it22@velhightech.com>
2025-01-28 21:50:47 +0530

    Adding numbers.java

--- Adding numbers.java
+++ Adding numbers.java
@@ -7,3 +7,14 @@ pubic class sum{
     System.out.println("Sum is: "+sum);
   }
 }
+// Adding 3 numbers
+
+pubic class sum{
+  public static void main(String []args){
+    int a = 5;
+    int b = 10;
+    int c = 20; 
+    int sum = a + b + c;
+    System.out.println("Sum is: "+sum);
+  }
+}
